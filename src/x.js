601ed845dd1d6bbc6f6836490dbba530d106ff0b
liersan <1031121541@qq.com>
2022-11-19 15:29:19 +0800

    准备提交了

--- src/x.js
+++ src/x.js
@@ -1,2 +1,4 @@
-import './x.css'
+import './y.scss'
+import './x.less'
+import './z.styl'
 export default 'xxx'
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const path = require('path');
 
 module.exports = {
     output: {
@@ -11,4 +11,44 @@ module.exports = {
             template: 'src/assets/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+
+            },
+            {
+                test: /\.styl$/,
+                loader: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader',
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ]
+            },
+
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    },
+                ],
+            },
+        ],
+    }
 };
+
